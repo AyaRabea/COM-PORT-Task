b77e3534352c1c713ce7c7a508328b0b09c909de
AyaRabea <ayarabea378@gmail.com>
2020-10-22 02:48:00 +0200

    COMPORT-Task

---
+++ ComPort1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ComPort1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.Send = new System.Windows.Forms.Button();
+            this.cboxportname = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(93, 97);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(131, 36);
+            this.Send.TabIndex = 0;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // cboxportname
+            // 
+            this.cboxportname.FormattingEnabled = true;
+            this.cboxportname.Location = new System.Drawing.Point(127, 35);
+            this.cboxportname.Name = "cboxportname";
+            this.cboxportname.Size = new System.Drawing.Size(121, 21);
+            this.cboxportname.TabIndex = 1;
+            this.cboxportname.Text = "COM2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "COM Names";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 244);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboxportname);
+            this.Controls.Add(this.Send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.ComboBox cboxportname;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ComPort1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ComPort1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //intialize portname & the remain configration use the defult 
+            //(we can edit BaudRate,Parity,..... from serialport setting in properties of visual studio)
+            string[] ports = SerialPort.GetPortNames();
+            cboxportname.Items.AddRange(ports);//load all ports number to the combox
+            serialPort1.PortName = cboxportname.Text;//user select value
+           
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (!serialPort1.IsOpen)//check if the serialport is not  open
+                {
+                    serialPort1.Open();
+                }
+                serialPort1.WriteLine("ABC"); //write to the port
+                serialPort1.Close();
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
